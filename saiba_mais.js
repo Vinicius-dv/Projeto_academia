de4cb4062201a909f9e0df6190832e463bfe936b
Vinicius-dv <150695214+Vinicius-dv@users.noreply.github.com>
2024-12-03 02:23:41 -0300

    Mais alguns ajustes

--- saiba_mais.js
+++ saiba_mais.js
@@ -7,38 +7,3 @@ menu_hamburguer.addEventListener('click',()=>{
     mobileIcon.classList.toggle("fa-x")
 })
 
-document.querySelector('.form_contato').addEventListener('submit',(e)=>{
-    e.preventDefault()
-
-    const nome = document.getElementById('nome').value
-    const email = document.getElementById('email').value
-    const message = document.getElementById('message').value
-
-    const dados = {
-        nome:nome,
-        email:email,
-        message:message
-    }
-
-    fetch('https://projeto-academia.onrender.com/contato',{
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify(dados)
-    })
-    .then(res=> res.json())
-    .then(data=>{
-        if (data.success) {
-            console.log('Mensagem enviada com sucesso')
-            document.querySelector('.form_contato').reset()
-        } else {
-            console.log('Falha no envio da mensagem:', data.message)
-        }
-    })
-    .catch(error => {
-        // Caso ocorra algum erro
-        alert('Erro ao enviar mensagem.');
-        console.error('Erro:', error);
-    });
-})
